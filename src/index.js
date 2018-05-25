36f76e67a83a8410ffade4a36844198ddcd7b7f8
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-05-25 08:53:37 -0400

    Agregué botones de cifrar y descifrar mensajes en html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 /* Acá va tu código */
+window.onload = function () {
+    /*Botones*/
+    const btn = document.getElementById('btn');
+    const btnIf = document.getElementById('btn-if');
+    const btnSwitch = document.getElementById('btn-switch');
+    const btnWhile = document.getElementById('btn-while');
+    const btnDoWhile = document.getElementById('btn-dowhile');
+    const btnFor = document.getElementById('btn-for');
