a4eb73b63dfdaf56d30b1efa688c7845322ed109
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-05-27 22:06:29 -0400

    js de cifrado añadida, falta hacer arreglos por particularidades de ascii

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,17 @@
 window.cipher = {
   encode: () => {
     /* Acá va tu código */
+    let palabraCifrada1 = [];
+    let palabraCifrada = [];
+    let desplazamiento = prompt('Ingrese desplazamiento: ');
+    let palabraInicial = prompt('Ingresar palabra: ');
+    let i = 0;
+    while (i < palabraInicial.length) {
+    palabraCifrada [i] = palabraInicial.charCodeAt(i) + parseInt(desplazamiento); /*Transforma char a ASCII + desplazamiento FUNCIONA!!*/ 
+    palabraCifrada1 [i] = String.fromCharCode(palabraCifrada[i]); /*Transforma ASCII a char (ya cifrada) FUNCIONA!! */
+    i++;
+    }
+    console.log(palabraCifrada1);
   },
   decode: () => {
     /* Acá va tu código */
