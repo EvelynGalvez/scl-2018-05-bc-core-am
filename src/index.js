ea36fbb2ce6464ab11d6cbcc1a6738eb0e93d890
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-05-31 10:21:11 -0400

    inicios en DOM mas cambios en style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 /* Acá va tu código */
 window.onload = function () {
     /*Botones*/
-    const btn = document.getElementById('btn');
-    const btnIf = document.getElementById('btn-if');
-    const btnSwitch = document.getElementById('btn-switch');
-    const btnWhile = document.getElementById('btn-while');
-    const btnDoWhile = document.getElementById('btn-dowhile');
-    const btnFor = document.getElementById('btn-for');
+    const codeButton = document.getElementById('btn-cifrar');
+    const encodeButton = document.getElementById('btn-descifrar');
+
+    codeButton.addEventListener('click', () => {
+        document.getElementById('mensajeUsuario').value;
+        document.getElementById('number').value;
+        ('mensajePrograma').innerHTML = window.cipher.encode(initialCode, displacement);   
+    });
